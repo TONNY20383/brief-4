6b45f38525070ba16ead69b2ba3a0965c46d025c
TONNY20383 <fokamtonny207@gmail.com>
2025-03-25 09:55:39 +0100

    Add files via upload

---
+++ brief - Copie/views/header.php
@@ -0,0 +1,21 @@
+<?php
+    require '../models/mod_head.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body class="font-sans">
+    <header class="flex justify-between bg-slate-600 px-4 py-2 w-full">
+        <h2 class="font-bold text-white text-3xl py-3">Digital free</h2>
+        <form action="#" method="post">
+            <button class="w-16 h-16 rounded-full bg-white" name="modifier"></button>
+        </form>
+    </header>
+</body>
+</html>
